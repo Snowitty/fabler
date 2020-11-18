46f275a0d027467c357d467aa30c56b0d2c2adc7
Snowitty <snowitty@163.com>
2020-11-18 15:31:15 +0800

    controller comment

--- internal/controller/rest/comment.go
+++ internal/controller/rest/comment.go
@@ -1,1 +1,195 @@
 package rest
+
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/snowitty/fabler/internal/h"
+	"github.com/snowitty/fabler/internal/model"
+)
+
+func GetComment(c *gin.Context) {
+
+	var co model.Comment
+	var err error
+
+	var id = c.Param("id")
+
+	co.ID, err = strconv.Atoi(id)
+	if err != nil {
+		c.JSON(200, h.Response{
+			Status:  500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if comment, err := co.Get(); err != nil {
+
+		c.JSON(200, h.Response{
+			Status:  404,
+			Message: err.Error(),
+		})
+
+	} else {
+
+		c.JSON(200, h.Response{
+			Status:  200,
+			Message: comment,
+		})
+
+	}
+
+	return
+}
+
+// UpdateComment func
+func UpdateComment(c *gin.Context) {
+
+	var co model.Comment
+	var ra int64
+	var err error
+
+	var id = c.Param("id")
+
+	co.ID, err = strconv.Atoi(id)
+	if err != nil {
+		c.JSON(200, h.Response{
+			Status:  500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if err = c.ShouldBindJSON(&co); err != nil {
+		c.JSON(200, h.Response{
+			Status:  500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if ra, err = co.Update(); err != nil {
+
+		c.JSON(200, h.Response{
+			Status:  404,
+			Message: err.Error(),
+		})
+
+	} else {
+
+		c.JSON(200, h.Response{
+			Status:  200,
+			Message: ra,
+		})
+	}
+
+	return
+}
+
+// DeleteComment func
+func DeleteComment(c *gin.Context) {
+
+	var co model.Comment
+	var ra int64
+	var err error
+
+	var id = c.Param("id")
+
+	co.ID, err = strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(200, h.Response{
+			Status:  500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if ra, err = co.Delete(); err != nil {
+		c.JSON(200, h.Response{
+			Status:  404,
+			Message: err.Error(),
+		})
+
+	} else {
+		c.JSON(200, h.Response{
+			Status:  200,
+			Message: ra,
+		})
+
+	}
+
+	return
+}
+
+// GetComments func
+func GetComments(c *gin.Context) {
+
+	var co model.Comment
+	var limit int
+	var offset int
+	var err error
+
+	answerID, _ := strconv.Atoi(c.Query("answerID"))
+	co.AnswerID = answerID
+
+	limit, err = strconv.Atoi(c.Query("limit"))
+	if err != nil {
+		c.JSON(200, h.Response{
+			Status:  500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	offset, err = strconv.Atoi(c.Query("offset"))
+	if err != nil {
+		c.JSON(200, h.Response{
+			Status:  500,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if comments, err := co.GetList(limit, offset); err != nil {
+		c.JSON(200, h.Response{
+			Status:  404,
+			Message: err.Error(),
+		})
+
+	} else {
+		c.JSON(200, h.Response{
+			Status:  200,
+			Message: comments,
+		})
+
+	}
+
+	return
+}
+
+// GetCommentsCounts func
+func GetCommentsCounts(c *gin.Context) {
+
+	var co model.Comment
+
+	answerID, _ := strconv.Atoi(c.Query("answerID"))
+	co.AnswerID = answerID
+
+	if counts, err := co.GetCounts(); err != nil {
+		c.JSON(200, h.Response{
+			Status:  404,
+			Message: err.Error(),
+		})
+
+	} else {
+		c.JSON(200, h.Response{
+			Status:  200,
+			Message: counts,
+		})
+
+	}
+
+	return
+}
