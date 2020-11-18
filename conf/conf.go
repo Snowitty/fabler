6a0e5159863df5845f18244aa0f25415696661b7
Snowitty <snowitty@163.com>
2020-11-18 14:01:41 +0800

    Config get instance

---
+++ conf/conf.go
@@ -0,0 +1,62 @@
+package conf
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"os"
+)
+
+type Configuration struct {
+	DB struct{
+		Driver string `json:"driver"`
+		Addr string `json:"addr"`
+	}`json:"db"`
+
+	Redis struct{
+		Addr string `json:"addr"`
+		Password string `json:"password"`
+		Db int `json:"db"`
+	}`json:"redis"`
+
+	Admin struct{
+		Name string `json:"name"`
+		Mail string `json:"mail"`
+		Password string `json:"password"`
+	}`json:"admin"`
+
+	Address string `json:"address"`
+	Lang string `json:"lang"`
+	Secretkey string `json:"secretkey"`
+}
+
+
+var conf *Configuration
+
+
+func Config() *Configuration{
+
+	if conf != nil{
+		return conf
+	}
+
+	var err error
+
+	viper.SetConfigFile("configuration")
+	viper.AddConfigPath("../fabler/")
+	viper.SetConfigType("yaml")
+
+	if err = viper.ReadInConfig(); err != nil {
+		fmt.Printf("config file error: %s\n", err)
+		os.Exit(1)
+	}
+	if err = viper.Unmarshal(&conf); err != nil {
+		fmt.Println("config file error: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Configuration.conf", conf)
+
+	return conf
+
+
+}
