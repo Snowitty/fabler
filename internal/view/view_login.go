eff86f25f8526cdf912a60a8df52de0891d1b2f8
Snowitty <snowitty@163.com>
2020-11-18 16:53:00 +0800

    view login

--- internal/view/view_login.go
+++ internal/view/view_login.go
@@ -1,1 +1,22 @@
 package view
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/snowitty/fabler/conf"
+	"net/http"
+)
+
+func Login(c *gin.Context){
+
+	var csrdata map[string]interface{}
+
+	template := "login.html"
+	data := map[string]interface{}{
+		"lang": conf.Config().Lang,
+		"csrdata": csrdata,
+	}
+
+	c.HTML(http.StatusOK, template, data)
+
+	return
+}
