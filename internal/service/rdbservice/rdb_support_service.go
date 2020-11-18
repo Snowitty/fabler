08cfdd17c5f4f47ad708f1151a6ee839cdbb33ae
Snowitty <snowitty@163.com>
2020-11-18 15:32:38 +0800

    service rdb service

--- internal/service/rdbservice/rdb_support_service.go
+++ internal/service/rdbservice/rdb_support_service.go
@@ -1,1 +1,99 @@
 package rdbservice
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/snowitty/fabler/internal/database"
+	"github.com/snowitty/fabler/internal/model"
+	"strconv"
+)
+
+var ctx = context.Background()
+
+func RedisAddSupporter(AnswerID int, MemberID int) (err error){
+
+	redisKey := getRedisKey(MemberID)
+	isMember, err := database.RDB.SIsMember(ctx, redisKey, AnswerID).Result()
+	if err != nil{
+		return
+	}
+
+	if isMember == true {
+		err = errors.New("Has Upvoted")
+		return
+	}
+
+	err = database.RDB.SAdd(ctx, redisKey, AnswerID).Err()
+	return
+
+}
+
+func RedisRemoveSupporter(AnswerID int, MemberID int) (err error){
+
+	redisKey := getRedisKey(MemberID)
+	isMember, err := database.RDB.SIsMember(ctx, redisKey, AnswerID).Result()
+	if err != nil{
+		return
+	}
+
+	if isMember == false{
+		err = errors.New("Hasn't Upvoted")
+		return
+	}
+
+	err = database.RDB.SRem(ctx, redisKey, AnswerID).Err()
+	return
+}
+
+func RedisWrapSupported(answer *model.Answer, MemberID int) (err error){
+	memMap, err := getRedisSmemMap(MemberID)
+	if err != nil{
+		return
+	}
+
+	IDString := strconv.Itoa(answer.ID)
+	answer.Supported = memMap[IDString]
+
+	return
+}
+
+func RedisWrapListSupported(answers []model.Answer, MemberID int) (err error){
+	meMap, err := getRedisSmemMap(MemberID)
+
+	if err != nil{
+		return
+	}
+
+	for key, answer := range answers{
+		IDString := strconv.Itoa(answer.ID)
+		answers[key].Supported = meMap[IDString]
+	}
+
+	return
+}
+
+func getRedisKey(MemberID int) (redisKey string){
+	redisKey = fmt.Sprintf("supporter:profile_id:%v", MemberID)
+	return
+}
+
+func getRedisSmemMap(MemberID int) (memMap map[string]bool, err error){
+
+	redisKey := getRedisKey(MemberID)
+	memList, err := database.RDB.SMembers(ctx, redisKey).Result()
+
+	if err != nil{
+		return
+	}
+
+	memMap = make(map[string]bool)
+	for _, _AnswerID := range  memList{
+		memMap[_AnswerID] = true
+	}
+
+	return
+}
+
+
+
