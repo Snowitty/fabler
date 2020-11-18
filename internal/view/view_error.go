8da18a3a68f816bb11604272eed94b6367f8b8cf
Snowitty <snowitty@163.com>
2020-11-18 16:52:53 +0800

    view error

--- internal/view/view_error.go
+++ internal/view/view_error.go
@@ -1,1 +1,20 @@
 package view
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/snowitty/fabler/conf"
+	"net/http"
+)
+
+func Error(c *gin.Context){
+	var csrdata map[string]interface{}
+
+	template := "error.html"
+	data := map[string]interface{}{
+		"lang": conf.Config().Lang,
+		"csrdata": csrdata,
+	}
+	c.HTML(http.StatusOK, template, data)
+
+	return
+}
