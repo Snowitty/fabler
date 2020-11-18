6a66731270a788216d4c920e1933e37112203fb6
Snowitty <snowitty@163.com>
2020-11-18 14:06:11 +0800

    GORMBase

---
+++ internal/model/base.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"time"
+	"github.com/jinzhu/gorm"
+)
+
+type GORMBase struct {
+	ID int `json:"id" gorm:"AUTO_INCREMENT"`
+	CreatedAt int64 `json:"created_at"`
+	UpdatedAt int64 `json:"updated_at"`
+}
+
+func (m *GORMBase) BeforeCreate(scope *gorm.Scope)error{
+	if m.UpdatedAt == 0{
+		scope.SetColumn("UpdatedAt", time.Now().Unix())
+
+	}
+
+	scope.SetColumn("CreatedAt", time.Now().Unix())
+
+	return nil
+}
+
+func (m *GORMBase) BeforeUpdate(scope *gorm.Scope)error{
+	scope.SetColumn("UpdatedAt", time.Now().Unix())
+	return nil
+}
