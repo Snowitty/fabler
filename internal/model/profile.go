b1e8bf5fec0b41df48d7e1d56d645700187ca76d
Snowitty <snowitty@163.com>
2020-11-19 11:45:19 +0800

    go fmt

--- internal/model/profile.go
+++ internal/model/profile.go
@@ -1,67 +1,66 @@
 package model
 
 import (
-	"github.com/snowitty/fabler/internal/database"
 	"log"
+
+	"github.com/snowitty/fabler/internal/database"
 )
 
 type Profile struct {
 	GORMBase
 	Name string `json:"name"`
 	Desc string `json:"desc"`
-
 }
 
-func (p *Profile) Get() (profile Profile,err error){
-	if err = database.DB.Where(&p).First(&profile).Error;err != nil{
+func (p *Profile) Get() (profile Profile, err error) {
+	if err = database.DB.Where(&p).First(&profile).Error; err != nil {
 		log.Print(err)
 	}
 
 	return
 }
 
-func (p *Profile) Create() (ra int64, err error){
-	if err = database.DB.Create(&p).Error;err != nil {
+func (p *Profile) Create() (ra int64, err error) {
+	if err = database.DB.Create(&p).Error; err != nil {
 		ra = -1
 		log.Print(err)
-	}else {
+	} else {
 		ra = 1
 	}
 	return
 }
 
-func (p *Profile) Update() (ra int64,err error){
-	if err = database.DB.Model(&p).Update(p).Error;err != nil{
+func (p *Profile) Update() (ra int64, err error) {
+	if err = database.DB.Model(&p).Update(p).Error; err != nil {
 		ra = -1
 		log.Print(err)
-	}else {
+	} else {
 		ra = 1
 	}
 	return
 }
 
-func (p *Profile) Delete() (ra int64,err error){
-	if err = database.DB.Delete(&p).Error;err != nil{
+func (p *Profile) Delete() (ra int64, err error) {
+	if err = database.DB.Delete(&p).Error; err != nil {
 		ra = -1
 		log.Print(err)
-	}else {
+	} else {
 		ra = 1
 	}
 	return
 }
 
-func (p *Profile) GetList(limit int, offset int) (profile []Profile, err error){
-	if err = database.DB.Offset(offset).Limit(limit).Find(&profile).Error;err != nil{
+func (p *Profile) GetList(limit int, offset int) (profile []Profile, err error) {
+	if err = database.DB.Offset(offset).Limit(limit).Find(&profile).Error; err != nil {
 		log.Print(err)
 	}
 	return
 }
 
-func (p *Profile) GetCounts() (counts int, err error){
-	if err = database.DB.Model(&Profile{}).Count(&counts).Error;err != nil{
+func (p *Profile) GetCounts() (counts int, err error) {
+	if err = database.DB.Model(&Profile{}).Count(&counts).Error; err != nil {
 		log.Print(err)
 	}
 
 	return
 }
-
